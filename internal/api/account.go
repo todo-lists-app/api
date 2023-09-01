7d28bcdd0a1a63d8db87e25e548c7c32e915c418
Keloran <keloran@develbox.info>
2023-09-01 16:09:55 +0100

    delete user works

--- internal/api/account.go
+++ internal/api/account.go
@@ -1,43 +1,57 @@
-// Package api is the service itself
 package api
 
 import (
 	"context"
-
+	"github.com/bugfixes/go-bugfixes/logs"
+	pb "github.com/todo-lists-app/protobufs/generated/user/v1"
 	"github.com/todo-lists-app/todo-lists-api/internal/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-// AccountDetails is the account details
-type AccountDetails struct {
-	Salt string `json:"salt,omitempty"`
+type AccountServiceClientCreator interface {
+	NewAccountServiceClient() pb.UserServiceClient
 }
 
-// Account is the account service
 type Account struct {
-	context.Context
 	config.Config
-	Subject string
+	context.Context
+	UserID      string
+	AccessToken string
+	Client      pb.UserServiceClient
 }
 
-// NewAccountService creates a new account service
-func NewAccountService(ctx context.Context, cfg config.Config, subject string) *Account {
+func NewAccountService(ctx context.Context, cfg config.Config, id, accessToken string) *Account {
 	return &Account{
-		Context: ctx,
-		Config:  cfg,
-		Subject: subject,
+		Config:      cfg,
+		Context:     ctx,
+		UserID:      id,
+		AccessToken: accessToken,
 	}
 }
 
-// GetAccount gets an account for the user
-func (a *Account) GetAccount() (*AccountDetails, error) {
-	return &AccountDetails{
-		Salt: "salt",
-	}, nil
+func (a *Account) GetClient() (*Account, error) {
+	conn, err := grpc.DialContext(a.Context, a.Config.Services.User, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, logs.Errorf("error dialing grpc: %v", err)
+	}
+
+	a.Client = pb.NewUserServiceClient(conn)
+	return a, nil
 }
 
-// CreateAccount creates an account for the user
-func (a *Account) CreateAccount() (*AccountDetails, error) {
-	return &AccountDetails{
-		Salt: "salt",
-	}, nil
+func (a *Account) DeleteAccount() error {
+	resp, err := a.Client.Delete(a.Context, &pb.UserDeleteRequest{
+		UserId:      a.UserID,
+		AccessToken: a.AccessToken,
+	})
+	if err != nil {
+		return logs.Errorf("error deleting account: %v", err)
+	}
+
+	if resp.GetStatus() != "ok" {
+		return logs.Errorf("error deleting account: %v", resp.GetStatus())
+	}
+
+	return nil
 }
